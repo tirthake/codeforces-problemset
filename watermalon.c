523403c20fd6023169469d10015f9fb1ce34066f
TIRTHA DAS <tirthake@gmail.com>
2025-04-29 17:21:20 +0600

    Add files via upload

---
+++ watermalon.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int w;
+
+    scanf("%d", &w);// without prompt
+
+    if (w > 2 && w % 2 == 0)
+
+        {
+        printf("YES\n");
+    }
+    else
+     {
+            printf("NO\n");
+    }
+
+    return 0;
+}
